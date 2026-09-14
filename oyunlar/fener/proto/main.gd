a4fd6d9e282b4b492e2f44b6f92f11f0cda80e0b
VolkaGames <volkanaybar45@gmail.com>
2026-09-14 22:39:03 +0300

    Otomatik commit: main.gd guncellendi - 2026-09-14 22:39

--- oyunlar/fener/proto/main.gd
+++ oyunlar/fener/proto/main.gd
@@ -194,6 +194,7 @@ func _process(delta: float) -> void:
 	queue_redraw()
 	for l in beam_layers:
 		l.queue_redraw()
+	rim_layer.queue_redraw()
 
 
 # --- ışın --------------------------------------------------------------------
