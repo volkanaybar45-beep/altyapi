8181ea39245f713b1c4dd9f793831e45e57f1c38
VolkaGames <volkanaybar45@gmail.com>
2026-09-14 21:09:22 +0300

    Otomatik commit: main.gd guncellendi - 2026-09-14 21:09

--- oyunlar/fener/proto/main.gd
+++ oyunlar/fener/proto/main.gd
@@ -43,7 +43,20 @@ func _ready() -> void:
 	title_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
 	title_label.position.x = 24
 	info_label = _make_label(36, Color(1.0, 0.93, 0.7), 1200)
+	mode_button = Button.new()
+	mode_button.position = MODE_RECT.position
+	mode_button.size = MODE_RECT.size
+	mode_button.add_theme_font_size_override("font_size", 30)
+	mode_button.focus_mode = Control.FOCUS_NONE
+	mode_button.pressed.connect(toggle_mode)
+	add_child(mode_button)
 	load_level(0)
+
+
+## 45° ↔ 90°. Bölüm baştan kurulur (aynalar başlangıç açısına döner).
+func toggle_mode() -> void:
+	mode90 = not mode90
+	load_level(level)
 
 
 func _make_label(font_size: int, color: Color, y: float) -> Label:
