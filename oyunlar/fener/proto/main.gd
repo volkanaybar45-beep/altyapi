f26a9523db4928341cdf87d12daa0f0d9bacc489
VolkaGames <volkanaybar45@gmail.com>
2026-09-14 22:34:05 +0300

    Otomatik commit: main.gd guncellendi - 2026-09-14 22:34

--- oyunlar/fener/proto/main.gd
+++ oyunlar/fener/proto/main.gd
@@ -52,6 +52,8 @@ var time := 0.0
 
 var title_label: Label
 var info_label: Label
+var beam_layers: Array = []
+var lantern := 0.0  # tamamlanınca tekne feneri parlaması (0..1)
 
 
 func _ready() -> void:
