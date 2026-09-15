c15af9929be9deb71db1d9aff4e951b3e4739167
VolkaGames <volkanaybar45@gmail.com>
2026-09-15 20:29:32 +0300

    Otomatik commit: main.gd guncellendi - 2026-09-15 20:29

--- oyunlar/fener/proto/main.gd
+++ oyunlar/fener/proto/main.gd
@@ -247,6 +247,7 @@ func load_level(i: int) -> void:
 	# ufuk ilk dolu satırın biraz üstünde: bütün nesneler suda durur
 	arka.horizon_y = origin.y + (top + 0.6 - 0.3) * cell
 	_place_moon()
+	_lit_count = -1  # yükleme anında çan çalmasın
 	update_ray()
 
 
