5f67504f7554e2255ba64559e1d0d8b02fe1a2eb
VolkaGames <volkanaybar45@gmail.com>
2026-09-15 20:36:54 +0300

    Otomatik commit: main.gd guncellendi - 2026-09-15 20:36

--- oyunlar/fener/proto/main.gd
+++ oyunlar/fener/proto/main.gd
@@ -605,10 +605,6 @@ func _sparkle(boat: Vector2) -> void:
 	p.finished.connect(p.queue_free)
 
 
-func _lantern_pos(boat: Vector2, sc: float = -1.0) -> Vector2:
-	return boat + (BOAT_LANTERN - Vector2(256, 256)) * (_boat_scale_px() if sc < 0.0 else sc)
-
-
 func _boat_scale_px() -> float:
 	return cell * BOAT_W / 490.0  # tekne görünür genişliği BOAT_W hücre (bbox 490)
 
